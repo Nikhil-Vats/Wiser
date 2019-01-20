36cbfeb101802c38e1abdb86cd2a034b5670b10f
NikhilPhalange <nikvats5499@gmail.com>
2019-01-20 15:45:54 +0530

    Basic home page completed

---
+++ js/index.js
@@ -0,0 +1,11 @@
+//show info
+
+function showInfo() {
+    var infoElement = document.getElementById('info');
+    infoElement.style.transform = 'scale(1)';
+}
+
+function closeInfo() {
+    var infoElement = document.getElementById('info');
+    infoElement.style.transform = 'scale(0)';
+}
