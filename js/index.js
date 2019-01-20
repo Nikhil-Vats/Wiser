93910e531ebc9a25c882e4cc4e5ed70978cf2f32
NikhilPhalange <nikvats5499@gmail.com>
2019-01-20 18:41:25 +0530

    Modified loader

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
-//show info
+// Hide Loader after page is loaded
+
+window.onload = function() { 
+                    document.getElementsByClassName("loading")[0].style.transform = 'scale(0)'; 
+                }   
+//Show info
 
 function showInfo() {
     var infoElement = document.getElementById('info');
-    infoElement.style.transform = 'scale(1)';
+    infoElement.style.transform = 'translateX(0) translateY(0) scale(1)';
 }
+
+//Close info
 
 function closeInfo() {
     var infoElement = document.getElementById('info');
-    infoElement.style.transform = 'scale(0)';
+    infoElement.style.transform = 'translateX(100vw) translateY(-100vh) scale(0)';
 }
