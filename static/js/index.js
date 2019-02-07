c75f81b467b43d1e69726ab6fbfb0784f057fd45
Arpit Anshuman <f2016250@pilani.bits-pilani.ac.in>
2019-02-07 21:31:03 +0530

    Bug Fixes

--- static/js/index.js
+++ static/js/index.js
@@ -73,3 +73,64 @@ setInterval(() => {
 function onSignIn() {
   window.location.href = "/accounts/google/login/?process=login";  
 }
+// Animation Code
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml8 .circle-white',
+    scale: [0, 3],
+    opacity: [1, 0],
+    easing: "easeInOutExpo",
+    rotateZ: 360,
+    duration: 1100
+  }).add({
+    targets: '.ml8 .circle-container',
+    scale: [0, 1],
+    duration: 1100,
+    easing: "easeInOutExpo",
+    offset: '-=1000'
+  }).add({
+    targets: '.ml8 .circle-dark',
+    scale: [0, 1],
+    duration: 1100,
+    easing: "easeOutExpo",
+    offset: '-=600'
+  }).add({
+    targets: '.ml8 .letters-left',
+    scale: [0, 1],
+    duration: 1200,
+    offset: '-=550'
+  }).add({
+    targets: '.ml8 .bang',
+    scale: [0, 1],
+    rotateZ: [45, 15],
+    duration: 1200,
+    offset: '-=1000'
+  }).add({
+    targets: '.ml8',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1400
+  });
+
+anime({
+  targets: '.ml8 .circle-dark-dashed',
+  rotateZ: 360,
+  duration: 8000,
+  easing: "linear",
+  loop: true
+});
+// Contact Section
+function openContact() {
+  document.getElementById('contact-us').style.display = 'block';
+}
+function closeContact() {
+  document.getElementById('contact-us').style.display = 'none';
+}
+// Devs Section
+function openDevs() {
+  document.getElementById('devs').style.display = 'block';
+}
+function closeDevs() {
+  document.getElementById('devs').style.display = 'none';
+}
